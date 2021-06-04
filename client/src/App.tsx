7461ffd0fcc99b8b0c66e47006af68358f5303bb
Filip Trplan <filip@trplan.si>
2021-06-04 19:11:17 +0200

    basic spa navigation

---
+++ client/src/App.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  NavLink,
+  Switch
+} from "react-router-dom";
+import { Files } from './pages/Files';
+import { Home } from "./pages/Home";
+import './styles/app.scss';
+
+interface AppProps { }
+
+class App extends React.PureComponent<AppProps> {
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <nav className="navbar">
+            <div className="logo-container">
+              <h1 className="name">
+                Files
+          </h1>
+            </div>
+            <ul className="nav-links">
+              <li className="nav-item is-active">
+                <NavLink exact to="/" className="nav-link" activeClassName="is-active">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/files" className="nav-link" activeClassName="is-active">Manage files</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className="content container">
+            <Switch>
+              <Route exact path="/" component={Home}>
+              </Route>
+              <Route exact path="/files" component={Files}>
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/pages/Files.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export class Files extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Files page</h1>
+      </div>
+    );
+}}
